3cf1d0b023771ab58d394d1997ac7b40ec768c5a
Рустем Гимаев <rustem.gimaev@bristolcapital.ru>
2025-03-12 21:03:34 +0500

    test

--- test_search_in_duckduckgo.py
+++ test_search_in_duckduckgo.py
@@ -4,9 +4,9 @@ from selene import browser, have
 # Поиск на DuckDuckGo
 def test_succesfull_register_click():
     browser.open('https://duckduckgo.com/')
-    browser.element('[type="text"]').type('https://jwt.io/')
+    browser.element('[type="text"]').type('https://jwt.is/')
     browser.element('[type="submit"]').click()
-    browser.element('[id="r1-0"]').should(have.text('JWT.IO'))
-    browser.element('[href="https://jwt.io/"]').click()
-    browser.element('[class="container"]').should(have.text('Debugger'))
+    browser.element('[id="r1-0"]').should(have.text('jwt.is'))
+    browser.element('[href="https://jwt.is/"]').click()
+    browser.element('[class="decoded"]').should(have.text('Decoded'))
     browser.quit()
